0e46d1c6ed0c0500993e2f1de9e564f86b6e2978
Christian Döring <christian.doering@tum.de>
2025-04-26 10:08:52 +0200

    Fixed template

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   outputs = {self, ...}: {
     templates = {
       mitsuba3 = {
-        path = "./mitsuba3";
+        path = ./mitsuba3;
         description = "Build environment for Dr.Jit and Mitsbua3";
       };
     };
